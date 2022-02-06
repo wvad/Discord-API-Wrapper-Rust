3da9f69dabf5f2a4efe2ecaeae77d3936ad61b44
csjsc <furandolltyan@gmail.com>
2022-02-06 12:54:31 +0900

    Update

--- tests/test.rs
+++ tests/test.rs
@@ -4,7 +4,7 @@ use discors::etf;
 
 #[tokio::test]
 async fn async_test() {
-  let mut client = Client::new("NzQxODg4NzQyNzc5OTEyMzIz.Xy-Huw.TtbpvL5-mUiJSshv7ElHdF8Y1Nc".to_string());
+  let mut client = Client::new("TOKEN HERE".to_string());
   println!("[INFO] Connection's error: {:?}", client.connect().await);
   while !client.shards.is_finished() {
     for message in client.shards.get_messages() {
